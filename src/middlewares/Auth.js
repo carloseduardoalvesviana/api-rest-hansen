293fee5bf2fa2260be632038237ae93164437d61
carloseduardoalvesviana <eduardostrink@gmail.com>
2021-07-24 00:24:01 -0300

    remove secret

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -30,7 +30,7 @@ module.exports = {
       });
 
     try {
-      jwt.verify(authorization, process.env.SECRET || '6428A8FBBB0E120528BB604A66787102280E1B5E26DB76633CE37C8A4526E445');
+      jwt.verify(authorization, process.env.SECRET);
       next();
     } catch (error) {
       return res.status(401).json({ message: 'Token Inválido!' });
